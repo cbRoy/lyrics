1a7e7643a2c9efc78ae861d8b7961ebf30adfdd5
Chris Roy <christofer.roy@gmail.com>
2016-06-27 22:14:12 -0400

    working

--- lyrics.py
+++ lyrics.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 
+import sys
+import pydoc
 import urllib2
-import re
-import os
 from bs4 import BeautifulSoup
 
-def getLyrics(search)
-    search = urllib.quote(search)
-    soup = BeautifulSoup(urllib2.urlopen("http://www.songlyrics.com/index.php?section=search&searchW="+search).read())
+def getLyrics(search):
+    search = urllib2.quote(search)
+    soup = BeautifulSoup(urllib2.urlopen("http://www.songlyrics.com/index.php?section=search&searchW="+search).read(),'lxml')
+    firstResult = soup.select_one(".serpresult h3 a")['href']
+    soup = BeautifulSoup(urllib2.urlopen(firstResult).read(),'lxml');
+    title = soup.title.string
+    lyrics = soup.select_one('#songLyricsDiv');
+    return title, lyrics.get_text()
+
+def getSearch():
+    return raw_input("Search: ")
+
+args = sys.argv
+search = ' '.join(args[1:])
+#search = getSearch()
+title, lyrics = getLyrics(search)
+print("\x1B]0;%s\x07" % title)
+pydoc.pager(lyrics)
