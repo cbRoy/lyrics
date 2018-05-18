213c5e80cec45250f84a74ca5119eefcf7075e96
Chris Roy <christofer.roy@gmail.com>
2018-05-17 20:04:24 -0400

    added user-agent to fix 403 errors

--- lyrics.py
+++ lyrics.py
@@ -7,9 +7,12 @@ from bs4 import BeautifulSoup
 
 def getLyrics(search):
     search = urllib2.quote(search)
-    soup = BeautifulSoup(urllib2.urlopen("http://www.songlyrics.com/index.php?section=search&searchW="+search).read(),'lxml')
+    url="http://www.songlyrics.com/index.php?section=search&searchW="+search;
+    req = urllib2.Request(url, headers={'User-Agent' : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"}) 
+    soup = BeautifulSoup(urllib2.urlopen( req ).read(),'lxml')
     firstResult = soup.select_one(".serpresult h3 a")['href']
-    soup = BeautifulSoup(urllib2.urlopen(firstResult).read(),'lxml');
+    req = urllib2.Request(firstResult, headers={'User-Agent' : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"}) 
+    soup = BeautifulSoup(urllib2.urlopen(req).read(),'lxml');
     title = soup.title.string
     lyrics = soup.select_one('#songLyricsDiv');
     return title, lyrics.get_text()
