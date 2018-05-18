2f39170328797847bd5cbf64ba09fc27e491cff8
Chris Roy <christofer.roy@gmail.com>
2018-05-18 18:29:12 -0400

    argument check added

--- lyrics.py
+++ lyrics.py
@@ -5,14 +5,19 @@ import pydoc
 import urllib2
 from bs4 import BeautifulSoup
 
+def createRequest(url):
+    return urllib2.Request(url, headers={'User-Agent' : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"}) 
+
+def getResult(url):
+    request = createRequest (url)
+    return BeautifulSoup(urllib2.urlopen( request ).read(),'lxml')
+
 def getLyrics(search):
     search = urllib2.quote(search)
     url="http://www.songlyrics.com/index.php?section=search&searchW="+search;
-    req = urllib2.Request(url, headers={'User-Agent' : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"}) 
-    soup = BeautifulSoup(urllib2.urlopen( req ).read(),'lxml')
+    soup = getResult (url)
     firstResult = soup.select_one(".serpresult h3 a")['href']
-    req = urllib2.Request(firstResult, headers={'User-Agent' : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"}) 
-    soup = BeautifulSoup(urllib2.urlopen(req).read(),'lxml');
+    soup = getResult(firstResult)
     title = soup.title.string
     lyrics = soup.select_one('#songLyricsDiv');
     return title, lyrics.get_text()
@@ -21,8 +26,7 @@ def getSearch():
     return raw_input("Search: ")
 
 args = sys.argv
-search = ' '.join(args[1:])
-#search = getSearch()
+search = ' '.join(sys.argv[1:]) if len(sys.argv) > 1 else getSearch()
 title, lyrics = getLyrics(search)
-print("\x1B]0;%s\x07" % title)
-pydoc.pager(lyrics)
+#print("\033]30;%s\007" % title) #sets title of Konsole, doesn't change back though. find new solution TODO
+pydoc.pager(title + '\n\n' + lyrics)
