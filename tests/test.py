6911cc09a4ffd969436b99c0fccea443b0cd7eb3
Chris Roy <christofer.roy@gmail.com>
2018-06-10 00:20:41 -0400

    fix for host with short hrefs

--- tests/test.py
+++ tests/test.py
@@ -5,12 +5,13 @@ from lyrics.providers import Provider
 
 class TestProvider(unittest.TestCase):
 
-    test_host = Provider("songlist", "http://www.songlyrics.com/index.php?section=search&searchW=", [".serpresult h3 a","#songLyricsDiv"])
+    def setUp(self):
+        self.test_host = Provider("songlist", "http://www.songlyrics.com","/index.php?section=search&searchW=", [".serpresult h3 a","#songLyricsDiv"])
     
-    def test_1(self):
-        print self.test_host.host
+    def test_host_name(self):
+        self.assertEqual(self.test_host.host, "songlist")
     
-    def test_2(self):
+    def test_getLyrics(self):
         print self.test_host.getLyrics('rag bone human')
         
 if __name__ == '__main__':
